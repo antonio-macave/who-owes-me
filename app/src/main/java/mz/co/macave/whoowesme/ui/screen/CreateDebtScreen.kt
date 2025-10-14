9e2712c6ea3df3a4c2dc27c4c116696b1f417312
António Macave <antonio.macave@gmail.com>
2025-10-14 10:31:05 +0200

    Name and Surname fields created

--- app/src/main/java/mz/co/macave/whoowesme/ui/screen/CreateDebtScreen.kt
+++ app/src/main/java/mz/co/macave/whoowesme/ui/screen/CreateDebtScreen.kt
@@ -1,5 +1,6 @@
 package mz.co.macave.whoowesme.ui.screen
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,9 +13,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import mz.co.macave.whoowesme.R
 
 @Composable
 fun CreateDebt() {
@@ -39,9 +44,51 @@ fun AmountField() {
             },
             value = amountText,
             singleLine = true,
-            label = { Text(text = "Amount") },
+            label = { Text(text = stringResource(R.string.amount)) },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             suffix = { Text(text = "MZN") }
+        )
+    }
+}
+
+@Composable
+fun NameFields() {
+    Column {
+
+        var name by remember { mutableStateOf("") }
+        var surname by remember { mutableStateOf("") }
+
+        TextField(
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+                .fillMaxWidth(),
+            value = name,
+            onValueChange = {
+                name = it
+            },
+            label = { Text(text = stringResource(R.string.name)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                capitalization = KeyboardCapitalization.Words,
+                imeAction = ImeAction.Next
+            ),
+        )
+
+        TextField(
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+                .fillMaxWidth(),
+            value = surname,
+            onValueChange = {
+                surname = it
+            },
+            label = { Text(text = stringResource(R.string.surname)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                capitalization = KeyboardCapitalization.Words
+            )
         )
     }
 }
