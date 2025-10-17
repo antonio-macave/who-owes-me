191dca9f9f3c8311b663880c13e7156d34f275e2
António Macave <antonio.macave@gmail.com>
2025-10-17 19:53:53 +0200

    Replacing TopAppBar by CenterAlignedTopAppBar.

--- app/src/main/java/mz/co/macave/whoowesme/ui/screen/AppBar.kt
+++ app/src/main/java/mz/co/macave/whoowesme/ui/screen/AppBar.kt
@@ -1,30 +1,23 @@
 package mz.co.macave.whoowesme.ui.screen
 
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun AppBar(title: String, onOkListener: () -> Unit, onCancelListener: () -> Unit) {
-    TopAppBar(
+    CenterAlignedTopAppBar(
         {
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                text = title,
-                textAlign = TextAlign.Center
-            )
+            Text(text = title)
         },
         navigationIcon = {
             IconButton(onClick = onCancelListener) {
