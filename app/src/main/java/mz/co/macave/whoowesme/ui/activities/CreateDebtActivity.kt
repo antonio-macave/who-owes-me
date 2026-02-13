e6bf9530bfc732fb6c7970c4a717c2819c522d16
António Macave <antonio.macave@gmail.com>
2026-02-13 12:34:36 +0200

    TopAppBar and screen content added

--- app/src/main/java/mz/co/macave/whoowesme/ui/activities/CreateDebtActivity.kt
+++ app/src/main/java/mz/co/macave/whoowesme/ui/activities/CreateDebtActivity.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import mz.co.macave.whoowesme.R
 import mz.co.macave.whoowesme.ui.activities.ui.theme.WhoOwesMeTheme
+import mz.co.macave.whoowesme.ui.screen.AppBar
+import mz.co.macave.whoowesme.ui.screen.CreateDebt
 
 class CreateDebtActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,8 +21,21 @@ class CreateDebtActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WhoOwesMeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        AppBar(
+                            title = stringResource(R.string.title_activity_create_debt),
+                            onCancelListener = { finish() },
+                            onOkListener = {  }
+                        )
+                    }
+                ) { innerPadding ->
+                    Column(
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        CreateDebt()
+                    }
                 }
             }
         }
