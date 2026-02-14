04ec009050b98fbc60d6ffce4ea9b48779bbb264
António Macave <antonio.macave@gmail.com>
2026-02-14 21:57:57 +0200

    DebtsScreen created

---
+++ app/src/main/java/mz/co/macave/whoowesme/ui/screen/DebtsScreen.kt
@@ -0,0 +1,155 @@
+package mz.co.macave.whoowesme.ui.screen
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import mz.co.macave.whoowesme.R
+import mz.co.macave.whoowesme.model.Debt
+import mz.co.macave.whoowesme.model.Debtor
+
+
+@Composable
+fun DebtsList(debts: List<Debt>, debtor: Debtor) {
+    LazyColumn {
+        itemsIndexed(items =  debts) { index,item_->
+            DebtorItem(
+                debt = debts[index],
+                debtor = debtor
+            )
+        }
+    }
+}
+
+@Composable
+fun DebtorItem(debt: Debt, debtor: Debtor) {
+    var visible by remember { mutableStateOf(false) }
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable(onClick = { visible = !visible })
+   ) {
+        Column() {
+            Row(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+            ) {
+                Column {
+                    Text(
+                        text = debtor.name,
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.Normal
+                    )
+                    Text(
+                        text = "${debt.status} MZN",
+                        style = MaterialTheme.typography.bodyLarge,
+                        //color = if (debt.amount >= 0) GoodSituation else RedSituation
+                    )
+                }
+            }
+            AnimatedVisibility(
+                visible = visible,
+                enter = fadeIn() + expandVertically(),
+                exit = fadeOut() + shrinkVertically(),
+            ) {
+                BottomButtons({}) { }
+            }
+
+        }
+
+    }
+}
+
+@Composable
+fun DebtInfo(debt: Debt) {
+
+}
+
+@Composable
+fun BottomButtons(onPrimaryClick: () -> Unit, onSecondaryClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.End
+    ) {
+        OutlinedButton(
+            onClick = onSecondaryClick
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(R.drawable.outline_time_24),
+                contentDescription = null
+            )
+            Spacer(Modifier.width(8.dp))
+            Text(text = stringResource(R.string.history))
+        }
+        Spacer(Modifier.width(16.dp))
+        Button(
+            onClick = onPrimaryClick
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = null
+            )
+            Spacer(Modifier.width(8.dp))
+            Text(text = stringResource(R.string.transaction))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Previews() {
+    //BottomButtons(onPrimaryClick = { }) { }
+    val debt = Debt(
+        id = 1,
+        status = 0,
+        description = "Car",
+        additionalNotes = "5 prestações",
+        amount = 20000.0,
+        debtorId = 1
+    )
+
+
+    val debtor = Debtor(
+        id = 2,
+        name = "Name",
+        surname = "Surname",
+        contactNumber = "821234567",
+
+    )
+    DebtorItem(debt = debt, debtor = debtor)
+}
