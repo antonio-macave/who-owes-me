fab8578748e7606ae881dbe04e354bfde617ff88
António Macave <antonio.macave@gmail.com>
2026-02-15 15:22:32 +0200

    AnimatedVisibility state created in View Model.

--- app/src/main/java/mz/co/macave/whoowesme/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/mz/co/macave/whoowesme/viewmodel/MainActivityViewModel.kt
@@ -10,7 +10,14 @@ class MainActivityViewModel: ViewModel() {
     private val _fabMenuExpanded = MutableStateFlow(false)
     val fabMenuExpanded: StateFlow<Boolean> get() = _fabMenuExpanded.asStateFlow()
 
+    private val _cardExpanded = MutableStateFlow(false)
+    val cardExpanded: StateFlow<Boolean> get() = _cardExpanded.asStateFlow()
+
     fun updateFabMenuExpanded(expanded: Boolean) {
         _fabMenuExpanded.value = expanded
+    }
+
+    fun updateCardExpanded(cardExpanded: Boolean) {
+        _cardExpanded.value = cardExpanded
     }
 }
