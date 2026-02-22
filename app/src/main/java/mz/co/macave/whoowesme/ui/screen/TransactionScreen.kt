142e12e911ed2bee5c14a61a88b3820e2894c2dc
António Macave <antonio.macave@gmail.com>
2026-02-22 12:23:02 +0200

    Optimizing imports

--- app/src/main/java/mz/co/macave/whoowesme/ui/screen/TransactionScreen.kt
+++ app/src/main/java/mz/co/macave/whoowesme/ui/screen/TransactionScreen.kt
@@ -1,17 +1,29 @@
 package mz.co.macave.whoowesme.ui.screen
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Today
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import mz.co.macave.whoowesme.R
 import mz.co.macave.whoowesme.model.Transaction
 
 @Composable
