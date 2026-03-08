acfd7cfc0ad9fcc8521cf9b4a39973f43d5a967f
António Macave <antonio.macave@gmail.com>
2026-03-08 20:20:29 +0200

    Extension Function added to format the currency

--- app/src/main/java/mz/co/macave/whoowesme/util/DateFormatterUtil.kt
+++ app/src/main/java/mz/co/macave/whoowesme/util/DateFormatterUtil.kt
@@ -2,6 +2,7 @@ package mz.co.macave.whoowesme.util
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import java.text.NumberFormat
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.Locale
@@ -13,4 +14,12 @@ fun formatDate(date: String): String {
 
     val parsedDate = LocalDate.parse(date, inputFormat)
     return parsedDate.format(outputFormat)
+}
+
+fun Double.toMzn(): String {
+    val numberFormat = NumberFormat.getNumberInstance(Locale.forLanguageTag("pt")).apply {
+        maximumFractionDigits = 2
+        minimumFractionDigits = 2
+    }
+    return "${numberFormat.format(this)} MZN"
 }
