9a7108668a707fda84ee0b79f82c5ebb0624df17
António Macave <antonio.macave@gmail.com>
2026-04-07 12:45:49 +0200

    Debt status info composable added

--- app/src/main/java/mz/co/macave/whoowesme/ui/screen/DebtsScreen.kt
+++ app/src/main/java/mz/co/macave/whoowesme/ui/screen/DebtsScreen.kt
@@ -17,3 +17,39 @@ fun DebtItem(debt: Debt) {
         }
     }
 }
+
+@Composable
+fun DebtStatus(debtStatus: Int) {
+
+    val debtStatusText = when (debtStatus) {
+        DebtStatus.PENDING.code -> stringResource(R.string.debt_status_pending)
+        DebtStatus.PAID.code -> stringResource(R.string.debt_status_paid)
+        DebtStatus.OVERDUE.code -> stringResource(R.string.debt_status_overdue)
+        else -> ""
+    }
+
+    val debtStatusIcon = when (debtStatus) {
+        DebtStatus.PENDING.code -> R.drawable.debt_status_pending
+        DebtStatus.PAID.code -> R.drawable.debt_status_paid
+        DebtStatus.OVERDUE.code -> R.drawable.debt_status_overdue
+        else -> 0
+    }
+
+    Row(
+        modifier = Modifier
+            .background(
+                color = MaterialTheme.colorScheme.surface,
+                shape = RoundedCornerShape(14.dp)
+            )
+            .padding(horizontal = 8.dp, vertical = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            modifier = Modifier.size(16.dp),
+            imageVector = ImageVector.vectorResource(id = debtStatusIcon),
+            contentDescription = null
+        )
+        Spacer(Modifier.width(4.dp))
+        Text(text = debtStatusText)
+    }
+}
