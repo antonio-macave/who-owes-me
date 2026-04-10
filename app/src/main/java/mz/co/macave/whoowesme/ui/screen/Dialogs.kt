96d980c8cfb000096198d17538ecf39eb82e92d4
António Macave <antonio.macave@gmail.com>
2026-04-10 11:27:30 +0200

    Dialog composable for sorting debts added.

---
+++ app/src/main/java/mz/co/macave/whoowesme/ui/screen/Dialogs.kt
@@ -0,0 +1,98 @@
+package mz.co.macave.whoowesme.ui.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialogDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+
+@Composable
+fun SortByDialog(isOpen: Boolean, onDismiss: () -> Unit) {
+    if (isOpen) {
+        Dialog(
+            onDismiss
+        ) {
+            SortByContent() { }
+        }
+    }
+}
+
+@Composable
+fun SortByContent(onDismiss: () -> Unit) {
+    val options = listOf("Nome", "Data", "Valor")
+    val (selectedOption, onOptionSelected) = remember { mutableStateOf(options[0]) }
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(16.dp),
+            text = "Ordenar por...",
+             color = AlertDialogDefaults.textContentColor,
+            style = MaterialTheme.typography.titleLarge
+        )
+        Spacer(Modifier.height(8.dp))
+        HorizontalDivider()
+        Column(modifier = Modifier.selectableGroup()) {
+            options.forEach { item ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .selectable(
+                            selected = (selectedOption == item),
+                            onClick = { onOptionSelected(item) },
+                            role = Role.RadioButton
+                        ),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RadioButton(
+                        selected = (selectedOption == item),
+                        onClick = { }
+                    )
+                    Text(text = item)
+                }
+            }
+            Spacer(Modifier.height(8.dp))
+            HorizontalDivider()
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal =16.dp, vertical = 8.dp),
+                horizontalArrangement = Arrangement.End
+            ) {
+                TextButton(
+                    onClick = { onDismiss() }
+                ) { Text(text = "Cancel") }
+                Spacer(Modifier.width(8.dp))
+                TextButton(
+                    onClick = { }
+                ) { Text(text = "Ok") }
+            }
+        }
+    }
+}
