8fe14271720f39e92489a3e1da043e4d2fd57567
António Macave <antonio.macave@gmail.com>
2026-04-18 17:02:53 +0200

    Name and Surname fields added

--- app/src/main/java/mz/co/macave/whoowesme/ui/screen/CreateDebtorScreen.kt
+++ app/src/main/java/mz/co/macave/whoowesme/ui/screen/CreateDebtorScreen.kt
@@ -1,8 +1,62 @@
 package mz.co.macave.whoowesme.ui.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import mz.co.macave.whoowesme.R
 
 @Composable
 fun CreateDebtorContent() {
 
+}
+
+@Composable
+fun NameAndSurnameFields() {
+
+    Column {
+        var name by remember { mutableStateOf("") }
+        var surname by remember { mutableStateOf("") }
+
+        TextField(
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+                .fillMaxWidth(),
+            value = name,
+            onValueChange = {
+                name = it
+            },
+            label = { Text(text = stringResource(R.string.name)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                capitalization = KeyboardCapitalization.Words,
+                imeAction = ImeAction.Next
+            ),
+        )
+
+        TextField(
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+                .fillMaxWidth(),
+            value = surname,
+            onValueChange = {
+                surname = it
+            },
+            label = { Text(text = stringResource(R.string.surname)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                capitalization = KeyboardCapitalization.Words
+            )
+        )
+    }
 }
