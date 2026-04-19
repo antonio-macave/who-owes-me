18a3236535a9153de584c94e2abdaee6c0a2ef85
António Macave <antonio.macave@gmail.com>
2026-04-19 18:46:12 +0200

    Converters for the LocalDate type.

--- app/src/main/java/mz/co/macave/whoowesme/data/AppDatabase.kt
+++ app/src/main/java/mz/co/macave/whoowesme/data/AppDatabase.kt
@@ -2,9 +2,14 @@ package mz.co.macave.whoowesme.data.dao
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import mz.co.macave.whoowesme.data.dao.DebtDao
+import mz.co.macave.whoowesme.data.dao.DebtorDao
+import mz.co.macave.whoowesme.data.dao.TransactionDao
 import mz.co.macave.whoowesme.model.Debt
 import mz.co.macave.whoowesme.model.Debtor
 import mz.co.macave.whoowesme.model.Transaction
+import mz.co.macave.whoowesme.util.Converters
 
 @Database(
     entities = [
@@ -14,6 +19,7 @@ import mz.co.macave.whoowesme.model.Transaction
     ],
     version = 1
 )
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun debtorDao(): DebtorDao
