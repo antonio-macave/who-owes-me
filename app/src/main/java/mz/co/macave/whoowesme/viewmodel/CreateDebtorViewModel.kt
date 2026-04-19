5df2d424ad3b9c5c9f97aa3f88d7657225551308
António Macave <antonio.macave@gmail.com>
2026-04-19 10:40:52 +0200

    Create Debtor ViewModel created

---
+++ app/src/main/java/mz/co/macave/whoowesme/viewmodel/CreateDebtorViewModel.kt
@@ -0,0 +1,19 @@
+package mz.co.macave.whoowesme.viewmodel
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class CreateDebtorViewModel : ViewModel() {
+
+    private val _name = MutableStateFlow("")
+    val name: StateFlow<String> = _name.asStateFlow()
+
+    private val _surname = MutableStateFlow("")
+    val surname: StateFlow<String> = _surname.asStateFlow()
+
+    private val _contactNumber = MutableStateFlow("")
+    val contactNumber: StateFlow<String> = _contactNumber.asStateFlow()
+
+}
