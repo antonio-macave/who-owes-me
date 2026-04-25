84c5eabde63abcd399505893f35ab5cd744fadd0
António Macave <antonio.macave@gmail.com>
2026-04-25 18:20:57 +0200

    Total Payment Switch added

--- app/src/main/java/mz/co/macave/whoowesme/ui/screen/AddTransactionScreen.kt
+++ app/src/main/java/mz/co/macave/whoowesme/ui/screen/AddTransactionScreen.kt
@@ -1,24 +1,34 @@
 package mz.co.macave.whoowesme.ui.screen
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.ButtonGroupDefaults
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.ToggleButton
 import androidx.compose.material3.ToggleButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
@@ -66,6 +76,31 @@ fun TransactionTypeSelector() {
                 )
             }
         }
+    }
+}
+
+@Composable
+fun TotalPaymentSwitch() {
+    var selected by remember { mutableStateOf(false) }
+    Row(
+        modifier = Modifier
+            .selectable(
+                selected = selected,
+                onClick = { selected = !selected },
+                role = Role.RadioButton
+            )
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = stringResource(R.string.full_payment),
+            style = MaterialTheme.typography.bodyLarge,
+        )
+        Spacer(Modifier.weight(1f))
+        Switch(
+            checked = selected,
+            onCheckedChange = { selected = !selected }
+        )
     }
 }
 
