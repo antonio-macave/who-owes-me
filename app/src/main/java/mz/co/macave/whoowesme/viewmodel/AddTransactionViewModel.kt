75c0ae79a26eabdf62517be0bf23744b9dd87d93
António Macave <antonio.macave@gmail.com>
2026-04-26 12:28:09 +0200

    States added to the Add Transaction ViewModel

--- app/src/main/java/mz/co/macave/whoowesme/viewmodel/AddTransactionViewModel.kt
+++ app/src/main/java/mz/co/macave/whoowesme/viewmodel/AddTransactionViewModel.kt
@@ -4,4 +4,40 @@ import androidx.lifecycle.ViewModel
 
 class AddTransactionViewModel : ViewModel() {
 
+    private val _amount = MutableStateFlow(0)
+    val amount: StateFlow<Int> get() = _amount.asStateFlow()
+
+    private val _transactionType = MutableStateFlow<Int?>(null)
+    val transactionType: StateFlow<Int?> get() = _transactionType.asStateFlow()
+
+    private val _description = MutableStateFlow("")
+    val description: StateFlow<String> get() = _description.asStateFlow()
+
+    private val _selectedOption = MutableStateFlow<Int?>(null)
+    val selectedOption: StateFlow<Int?> get() = _selectedOption.asStateFlow()
+
+    private val _isTotalPayment = MutableStateFlow(false)
+    val isTotalPayment: StateFlow<Boolean> get() = _isTotalPayment.asStateFlow()
+
+    fun updateIsTotalPayment(value: Boolean) {
+        _isTotalPayment.value = value
+    }
+
+    fun updateAmount(value: Int) {
+        _amount.value = value
+    }
+
+    fun updateTransactionType(value: Int) {
+        _transactionType.value = value
+    }
+
+    fun updateDescription(value: String) {
+        _description.value = value
+    }
+
+    fun updateSelectedOption(value: Int) {
+        _selectedOption.value = value
+    }
+
+
 }
