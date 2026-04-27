3882021b61f382873949b53f6ab1531e3367ca00
António Macave <antonio.macave@gmail.com>
2026-04-27 11:12:01 +0200

    Setting up the Top App Bar and Activity content

--- app/src/main/java/mz/co/macave/whoowesme/ui/activities/AddTransactionActivity.kt
+++ app/src/main/java/mz/co/macave/whoowesme/ui/activities/AddTransactionActivity.kt
@@ -4,10 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import mz.co.macave.whoowesme.R
+import mz.co.macave.whoowesme.data.DatabaseProvider
+import mz.co.macave.whoowesme.data.repository.TransactionRepository
 import mz.co.macave.whoowesme.ui.activities.ui.theme.WhoOwesMeTheme
+import mz.co.macave.whoowesme.ui.screen.AddTransactionContent
+import mz.co.macave.whoowesme.ui.screen.AppBar
+import mz.co.macave.whoowesme.viewmodel.AddTransactionViewModel
+import mz.co.macave.whoowesme.viewmodel.ViewModelFactory
 
 class AddTransactionActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,8 +34,25 @@ class AddTransactionActivity : ComponentActivity() {
             val viewModel: AddTransactionViewModel by viewModels { factory }
 
             WhoOwesMeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        AppBar(
+                            title = stringResource(R.string.title_activity_add_transaction),
+                            onCancelListener = { finish() },
+                            onOkListener = {
 
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+                    Column(
+                        Modifier
+                            .padding(innerPadding)
+                            .padding(16.dp)
+                    ) {
+                        AddTransactionContent(viewModel)
+                    }
                 }
             }
         }
