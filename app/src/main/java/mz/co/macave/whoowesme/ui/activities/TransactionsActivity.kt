eaa65ba974ffb83a0fcade83d27c4bdc469d42a9
António Macave <antonio.macave@gmail.com>
2026-04-27 10:46:15 +0200

    Actions added to the TopBar

--- app/src/main/java/mz/co/macave/whoowesme/ui/activities/TransactionsActivity.kt
+++ app/src/main/java/mz/co/macave/whoowesme/ui/activities/TransactionsActivity.kt
@@ -1,21 +1,27 @@
 package mz.co.macave.whoowesme.ui.activities
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import mz.co.macave.whoowesme.R
 import mz.co.macave.whoowesme.ui.theme.WhoOwesMeTheme
@@ -51,6 +57,35 @@ fun TransactionsTopAppBar(title: String?, onNavigationButtonClick: () -> Unit) {
             IconButton(onClick = onNavigationButtonClick) {
                 Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
             }
-        }
+        },
+        actions = {
+
+            IconButton(
+                onClick = {
+
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.PersonOutline,
+                    contentDescription = stringResource(R.string.profile)
+                )
+            }
+
+            IconButton(
+                onClick = {
+                    val intent = Intent(context, AddTransactionActivity::class.java)
+                    context.startActivity(intent)
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(R.string.title_activity_add_transaction)
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary
+        )
     )
 }
