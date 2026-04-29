52f925fd80264df978a2075e19a8d216a8b7186a
António Macave <antonio.macave@gmail.com>
2026-04-29 20:36:06 +0200

    Leading icon added

--- app/src/main/java/mz/co/macave/whoowesme/ui/screen/CreateDebtScreen.kt
+++ app/src/main/java/mz/co/macave/whoowesme/ui/screen/CreateDebtScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.ButtonGroupDefaults
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
@@ -39,6 +40,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.role
 import androidx.compose.ui.semantics.semantics
@@ -133,7 +135,13 @@ fun ExistingDebtorSelector(viewModel: CreateDebtViewModel) {
                 label = { Text(text = stringResource(R.string.debtor)) },
                 singleLine = true,
                 readOnly = true,
-                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) }
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Person,
+                        contentDescription = null
+                    )
+                }
             )
 
             ExposedDropdownMenu(
