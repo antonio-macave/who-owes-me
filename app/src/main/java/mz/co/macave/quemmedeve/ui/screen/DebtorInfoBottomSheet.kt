c9767128832c03df060519e3897660de7eb3f434
António Macave <antonio.macave@gmail.com>
2026-06-04 22:41:36 +0200

    TotalAndPaidAmount Composable created.

--- app/src/main/java/mz/co/macave/quemmedeve/ui/screen/DebtorInfoBottomSheet.kt
+++ app/src/main/java/mz/co/macave/quemmedeve/ui/screen/DebtorInfoBottomSheet.kt
@@ -1,2 +1,84 @@
 package mz.co.macave.quemmedeve.ui.screen
 
+
+@Composable
+fun TotalAndPaiAmount(
+    totalDebt: Double,
+    paidAmount: Double,
+    debtsQuantity: Int
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Min)
+    ) {
+
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        color = MaterialTheme.colorScheme.surface,
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.total_debt),
+                    style = MaterialTheme.typography.bodySmall
+                )
+                Spacer(Modifier.height(8.dp))
+                Text(
+                    text = totalDebt.toMzn(),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+            Spacer(Modifier.height(8.dp))
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        color = MaterialTheme.colorScheme.surface,
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.paid_amount),
+                    style = MaterialTheme.typography.bodySmall
+                )
+                Spacer(Modifier.height(8.dp))
+                Text(
+                    text = paidAmount.toMzn(),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+
+        }
+
+        Spacer(Modifier.width(8.dp))
+
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .background(
+                    color = MaterialTheme.colorScheme.surface,
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = stringResource(R.string.debts),
+                style = MaterialTheme.typography.bodySmall
+            )
+            Spacer(Modifier.height(8.dp))
+            Text(
+                text = "$debtsQuantity",
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
+}
