36d3f5a4be15c96e1ee316d0f1ffc87acc15716c
António Macave <antonio.macave@gmail.com>
2026-06-21 20:46:55 +0200

    `sortedByOption` extension function added to `List<DebtCardItem>` to support sorting by name, date, and amount.

--- app/src/main/java/mz/co/macave/quemmedeve/extensions/DebtExtensions.kt
+++ app/src/main/java/mz/co/macave/quemmedeve/extensions/DebtExtensions.kt
@@ -1,2 +1,14 @@
 package mz.co.macave.quemmedeve.extensions
 
+import mz.co.macave.quemmedeve.model.DebtCardItem
+import mz.co.macave.quemmedeve.util.SortOption
+
+fun List<DebtCardItem>.sortedByOption(
+    sortOption: SortOption
+): List<DebtCardItem> {
+    return when (sortOption) {
+        SortOption.NAME -> sortedBy { it.debtorName }
+        SortOption.DATE -> sortedBy { it.dueTo }
+        SortOption.AMOUNT -> sortedBy { it.amount }
+    }
+}
