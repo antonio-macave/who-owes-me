26f1bdfd1a8a1ecdb4432a2e270ad330749a81f1
António Macave <antonio.macave@gmail.com>
2026-06-25 12:00:01 +0200

    StateFlows and update functions for `showDebtorBottomSheet` and `selectedDebtorWithDebts` added to `DebtorsActivityViewModel`.

--- app/src/main/java/mz/co/macave/quemmedeve/viewmodel/DebtorsActivityViewModel.kt
+++ app/src/main/java/mz/co/macave/quemmedeve/viewmodel/DebtorsActivityViewModel.kt
@@ -11,13 +11,22 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 import mz.co.macave.quemmedeve.data.repository.DebtorRepository
+import mz.co.macave.quemmedeve.model.Debt
 import mz.co.macave.quemmedeve.model.Debtor
 import mz.co.macave.quemmedeve.model.DebtorWithDebts
+import java.time.LocalDate
 
 class DebtorsActivityViewModel(val debtorsRepository: DebtorRepository) : ViewModel() {
 
     private val _cardExpanded = MutableStateFlow<Int?>(null)
     val cardExpanded: StateFlow<Int?> get() = _cardExpanded.asStateFlow()
+
+    private val _showDebtorBottomSheet = MutableStateFlow(false)
+    val showDebtorBottomSheet: StateFlow<Boolean> get() = _showDebtorBottomSheet.asStateFlow()
+
+    private val _selectedDebtorWithDebts = MutableStateFlow<DebtorWithDebts?>(null)
+    val selectedDebtorWithDebts: StateFlow<DebtorWithDebts?> get() = _selectedDebtorWithDebts.asStateFlow()
+
 
     private val _isLoading = MutableStateFlow(true)
     val isLoading: StateFlow<Boolean> get() = _isLoading.asStateFlow()
@@ -33,6 +42,13 @@ class DebtorsActivityViewModel(val debtorsRepository: DebtorRepository) : ViewMo
         _isLoading.value = false
     }
 
+    fun updateShowDebtorBottomSheet(newValue: Boolean) {
+        _showDebtorBottomSheet.value = newValue
+    }
+
+    fun updateSelectedDebtorWithDebts(debtorWithDebts: DebtorWithDebts) {
+        _selectedDebtorWithDebts.value = debtorWithDebts
+    }
 
     fun updateCardExpanded(id: Int) {
         _cardExpanded.value = if (_cardExpanded.value == id) null else id
