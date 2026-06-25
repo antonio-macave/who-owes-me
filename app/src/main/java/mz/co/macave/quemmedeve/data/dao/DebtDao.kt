e196d1efd956767473b769b6aa6d8a4e175e6af4
António Macave <antonio.macave@gmail.com>
2026-06-25 11:59:04 +0200

    `findDebtsWithDebtorNameByDebtorId` query and method added to `DebtDao`.

--- app/src/main/java/mz/co/macave/quemmedeve/data/dao/DebtDao.kt
+++ app/src/main/java/mz/co/macave/quemmedeve/data/dao/DebtDao.kt
@@ -36,6 +36,26 @@ interface DebtDao {
     @Query("SELECT * FROM debts WHERE id IN (:debtIds)")
     suspend fun loadAllDebtsById(debtIds: IntArray): List<Debt>
 
+    @Query("""
+        SELECT 
+            debts.id AS debtId, 
+            debtors.id AS debtorId, 
+            debtors.name AS debtorName,
+            debtors.surname AS debtorSurname,
+            debts.amount AS amount,
+            debts.paidAmount AS paidAmount,
+            debts.status AS status,
+            debts.description AS description,
+            debts.additionalNotes AS additionalNotes,
+            debts.dueTo AS dueTo
+        FROM debts
+        INNER JOIN debtors ON debts.debtorId = debtors.id
+        WHERE debtors.id IN (:debtorId)
+        ORDER BY dueTo ASC
+        """)
+    fun findDebtsWithDebtorNameByDebtorId(debtorId: IntArray): Flow<List<DebtCardItem>>
+
+
      @Query("SELECT * FROM debts WHERE id = :debtId")
      suspend fun findDebtById(debtId: Int): Debt
 
